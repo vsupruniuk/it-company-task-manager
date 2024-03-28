1c89fc626f8b27e9a22524b51c2bd96a76773540
Vladyslav Supruniuk <Vladyslav.Supruniuk@contracted.pmi.com>
2024-03-28 23:36:02 +0200

    Base project settings and global urls setup

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,6 +1,10 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("__debug__/", include("debug_toolbar.urls")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
