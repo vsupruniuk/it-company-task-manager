9d5c4337f755ebc8f54c7a9a8308532a58f898ff
Vladyslav Supruniuk <Vladyslav.Supruniuk@contracted.pmi.com>
2024-04-01 17:38:16 +0300

    Fix Tag model

--- manager/models/tag.py
+++ manager/models/tag.py
@@ -4,14 +4,15 @@ from django.db import models
 class Tag(models.Model):
     name = models.CharField(max_length=63)
 
+    project = models.ForeignKey(to="Project", on_delete=models.CASCADE, null=True)
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ["name"]
-        # TODO name + project
-        index_together = []
-        unique_together = []
+        index_together = ["name", "project"]
+        unique_together = ["name", "project"]
 
     def __str__(self) -> str:
-        return f"Task type: {self.name}"
+        return f"Tag: {self.name}"
